8a58a8925a43af2ac657a61578243780f3b0476b
Fionaverasha1234 <90539113+Fionaverasha1234@users.noreply.github.com>
2024-12-15 16:57:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ buyNowButton.addEventListener('click', () => {
   alert('Purchase successful! Thank you for your order.');
   cart = []; // Clear cart
   updateCartTable();
-  window.location.href = 'payment.html';
+  window.location.href = 'Payment.html';
 });
 
 // Initialize Application
